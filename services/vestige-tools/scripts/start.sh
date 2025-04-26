6b860339435aedfadca0a4323d74e59d0f934430
Exodation <oficialexodation@gmail.com>
2025-04-27 00:22:46 +0100

    Initial Tests

---
+++ services/vestige-tools/scripts/start.sh
@@ -0,0 +1,27 @@
+
+echo "Checking services..."
+
+for servicedir in /usr/local/bin/vestige-tools/*; do
+  if [ -d "$servicedir" ]; then
+    service_file=$(find "$servicedir" -maxdepth 1 -name "*.service")
+    
+    if [ -f "$service_file" ]; then
+
+      # Install the service
+      echo "Installing service $service_file..."
+      cp "$service_file" /etc/systemd/system/
+
+      # Enable the service
+      echo "Enabling service: $(basename $service_file)"
+      sudo systemctl enable "$(basename $service_file)"
+
+      # Start the service
+      echo "Starting service: $(basename $service_file)"
+      sudo systemctl start "$(basename $service_file)"
+    fi
+  fi
+done
+
+sudo systemctl daemon-reload
+
+echo "Vestige tools setup complete"
