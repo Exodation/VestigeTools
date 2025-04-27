7e0816886d9b86351a9e7120a7afe3d52ca280ab
Exodation <oficialexodation@gmail.com>
2025-04-27 01:06:30 +0100

    Setup changes 3

--- services/vestige-tools/init.sh
+++ services/vestige-tools/init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo ln -s /usr/local/bin/vestige-tools/services/vestige-tools/exec.sh /usr/local/bin/vestige-tools/services/vestige-tools/scripts
 
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 echo "Installing vestige tools..."
 
+rm -rf /usr/local/bin/vestige-tools
 mkdir -p /usr/local/bin/vestige-tools
 
 cp -r ./ /usr/local/bin/vestige-tools
