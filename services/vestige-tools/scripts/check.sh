f7913f6f53e27d09111261b3fccaff044a523df1
Exodation <oficialexodation@gmail.com>
2025-04-27 02:17:53 +0100

    Command output changes

--- services/vestige-tools/scripts/check.sh
+++ services/vestige-tools/scripts/check.sh
@@ -1,1 +1,13 @@
 echo 'Vestige tools running fine'
+
+cd /opt/VestigeTools/
+git fetch
+
+    if git diff --quiet HEAD..origin/main; then
+
+      echo "You are up to date"
+
+    else
+
+      echo "There is a more recent version. Run vestige-tools update to install"
+    fi
--- services/vestige-tools/scripts/update.sh
+++ services/vestige-tools/scripts/update.sh
@@ -2,3 +2,5 @@ cd /opt/VestigeTools/
 git reset --hard origin/main
 git pull
 chmod -R +x services/
+
+echo "Sucessfully updated to the most recent version"
