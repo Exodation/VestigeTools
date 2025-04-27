925386b3197c4090d776666e1f648bfeab03a0e5
Exodation <oficialexodation@gmail.com>
2025-04-27 13:32:52 +0100

    Syntax fixes

--- services/mysql-backup/init.sh
+++ services/mysql-backup/init.sh
@@ -4,10 +4,10 @@ MYSQL_USER=$(jq -r '.MYSQL_USER' config.json)
 MYSQL_PASSWORD=$(jq -r '.MYSQL_PASSWORD' config.json)
 MYSQL_DATABASE=$(jq -r '.MYSQL_DATABASE' config.json)
 
-if [ -n "$MYSQL_USER" ];
-docker exec mysql mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > backup.sql
+if [ -n "$MYSQL_USER" ]; then
+    docker exec mysql mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > backup.sql
 
-rclone copy backup.sql Backups:backups
+    rclone copy backup.sql Backups:backups
 else
     echo "Credentials not set, ignoring backup"
 fi
