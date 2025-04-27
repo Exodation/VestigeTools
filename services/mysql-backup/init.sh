20cbbec3e4816bbb1613afeaa2a1496cb71035b5
Exodation <oficialexodation@gmail.com>
2025-04-27 13:43:47 +0100

    Command build fixes + var fixes

--- services/mysql-backup/init.sh
+++ services/mysql-backup/init.sh
@@ -5,9 +5,13 @@ MYSQL_PASSWORD=$(jq -r '.MYSQL_PASSWORD' config.json)
 MYSQL_DATABASE=$(jq -r '.MYSQL_DATABASE' config.json)
 
 if [ -n "$MYSQL_USER" ]; then
+    echo "Backing up $MYSQL_DATABASE..."
+
     docker exec mysql mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > backup.sql
 
     rclone copy backup.sql Backups:backups
+    
+    echo "Sucessfully backed up database $MYSQL_DATABASE"
 else
     echo "Credentials not set, ignoring backup"
 fi
--- services/vestige-tools/scripts/start.sh
+++ services/vestige-tools/scripts/start.sh
@@ -7,7 +7,7 @@ for servicedir in /opt/VestigeTools/services/*; do
 
   if [ -d "$servicedir" ]; then
     service_file=$(find "$servicedir" -maxdepth 1 -name "*.service")
-    service_name=$(basename $service_file)
+    service_name=$(basename "$service_file")
 
     if [ -f "$service_file" ]; then
       # START SERVICES
@@ -19,25 +19,25 @@ for servicedir in /opt/VestigeTools/services/*; do
       cp "$service_file" /etc/systemd/system/
 
       # Enable the service
-      echo "Enabling service: $service_name"
       sudo systemctl enable "$service_name"
 
       # Start the service
-      echo "Starting service: $service_name"
       sudo systemctl start "$service_name"
-    else
 
-        # CUSTOM INIT SCRIPTS
-        init_sh=$(find "$servicedir" -maxdepth 1 -name "init.sh")
-        if [ -f "$init_sh" ]; then
-            echo "Initializing $(basename $service_file)"
-            ./$init_sh
-        else
-            exec_sh=$(find "$servicedir" -maxdepth 1 -name "exec.sh")
-            if [ -f "$exec_sh" ]; then
-              $COMMAND_BUILD $exec_sh $service_name
-            fi
-        fi
+      echo "Started service $service_name"
+    fi
+
+    # CUSTOM INIT SCRIPTS
+    init_sh=$(find "$servicedir" -maxdepth 1 -name "init.sh")
+    if [ -f "$init_sh" ]; then
+        echo "Initializing $(basename $service_file)"
+        ./$init_sh
+    fi
+
+    # CUSTOM COMMANDS
+    exec_sh=$(find "$servicedir" -maxdepth 1 -name "exec.sh")
+    if [ -f "$exec_sh" ]; then
+      $COMMAND_BUILD $exec_sh $service_name
     fi
   fi
 done
