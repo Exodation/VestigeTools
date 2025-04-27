f88e31fa7dd49154744a99e9acf73f28c623bddb
Exodation <oficialexodation@gmail.com>
2025-04-27 01:34:01 +0100

    Trying to understand links

--- setup.sh
+++ setup.sh
@@ -8,7 +8,3 @@ mkdir -p /usr/local/bin/vestige-tools
 cp -r ./ /usr/local/bin/vestige-tools
 
 cd /usr/local/bin/vestige-tools
-
-chmod +x ./services/vestige-tools/scripts/start.sh
-
-./services/vestige-tools/scripts/start.sh
