34e78c1542e56a77160af6228d3a317d18cbefcd
Exodation <oficialexodation@gmail.com>
2025-04-27 01:56:27 +0100

    Permission fixes

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ echo "Installing vestige tools..."
 
 chmod +x ./services/vestige-tools/scripts/start.sh
 
-ln -s /opt/VestigeTools/services/vestige-tools/exec.sh /usr/local/bin/vestige-tools
+ln -sf /opt/VestigeTools/services/vestige-tools/exec.sh /usr/local/bin/vestige-tools
+
+chmod +x /usr/local/bin/vestige-tools
 
 ./services/vestige-tools/scripts/start.sh
