657b4894ca4e43dd67955425e89440f35f03e34b
Exodation <oficialexodation@gmail.com>
2025-04-27 13:47:19 +0100

    Startup fixes

--- services/vestige-tools/scripts/start.sh
+++ services/vestige-tools/scripts/start.sh
@@ -7,7 +7,7 @@ for servicedir in /opt/VestigeTools/services/*; do
 
   if [ -d "$servicedir" ]; then
     service_file=$(find "$servicedir" -maxdepth 1 -name "*.service")
-    service_name=$(basename "$service_file")
+    service_name=$(basename "$servicedir")
 
     if [ -f "$service_file" ]; then
       # START SERVICES
@@ -25,18 +25,21 @@ for servicedir in /opt/VestigeTools/services/*; do
       sudo systemctl start "$service_name"
 
       echo "Started service $service_name"
+    else
+      # CUSTOM INIT SCRIPTS
+      init_sh=$(find "$servicedir" -maxdepth 1 -name "init.sh")
+      if [ -f "$init_sh" ]; then
+          echo "Initializing $(basename $service_file)"
+          ./$init_sh
+      fi
     fi
 
-    # CUSTOM INIT SCRIPTS
-    init_sh=$(find "$servicedir" -maxdepth 1 -name "init.sh")
-    if [ -f "$init_sh" ]; then
-        echo "Initializing $(basename $service_file)"
-        ./$init_sh
-    fi
+    
 
     # CUSTOM COMMANDS
     exec_sh=$(find "$servicedir" -maxdepth 1 -name "exec.sh")
     if [ -f "$exec_sh" ]; then
+      echo "Attemping $service_name"
       $COMMAND_BUILD $exec_sh $service_name
     fi
   fi
